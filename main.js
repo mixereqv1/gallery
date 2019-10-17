2a7e26394c47f2e65ad59a5a0a5b6a13a2030610
mixereqv1 <mateuszral7@gmail.com>
2019-10-17 23:48:42 +0200

    a

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 const images = ["BMW-2-series.jpg", "landscape01.jpg", "landscape02.jpg", "pingwin02.png", "red-car-4.jpg", "red-car-facebook-covers.jpg"];
 const main = document.querySelector('.main');
-const previous_button = document.querySelector('.previous');
-const next_button = document.querySelector('.next');
-const overlay_image = document.querySelector('.overlay__image');
+const previous_image = document.querySelector('.previous');
+const next_image = document.querySelector('.next');
+const photos = document.querySelector('.photos');
+const image = document.querySelector('.image');
+const current_image_span = document.querySelector('.current__image span');
 
 let current_image = 0;
 
-images.forEach( (element,i) => {
-    let img = document.createElement('img');
-    img.src = `images/${element}`;
-    img.className = 'image';
-    img.addEventListener('click', event => {
-        overlay_image.src = `images/${element}`;
-        current_image = i;
-    })
-    main.appendChild(img);
-})
+image.src = `images/${images[current_image]}`
+current_image_span.innerText = `${current_image+1} / ${images.length}`;
 
-previous_button.addEventListener('click', event => {
-    if(current_image > 0) {
+previous_image.addEventListener('click', event => {
+    if(current_image == 1) {
+        previous_image.classList.add('hidden');
+    }
+    image.style.opacity = 0;
+    setTimeout(a => {
         current_image--;
-        overlay_image.src = `images/${images[current_image]}`;
+        image.src = `images/${images[current_image]}`;
+        image.style.opacity = 1;
+        current_image_span.innerText = `${current_image+1} / ${images.length}`;
+    }, 300)
+    if(current_image == images.length - 1) {
+        next_image.classList.remove('hidden');
     }
 });
 
-next_button.addEventListener('click', event => {
-    if(current_image < images.length - 1) {
+next_image.addEventListener('click', event => {
+    previous_image.classList.remove('hidden');
+    image.style.opacity = 0;
+    setTimeout(a => {
         current_image++;
-        overlay_image.src = `images/${images[current_image]}`;
+        image.src = `images/${images[current_image]}`;
+        image.style.opacity = 1;
+        current_image_span.innerText = `${current_image+1} / ${images.length}`;
+    }, 300)
+    if(current_image == images.length - 2) {
+        next_image.classList.add('hidden');
     }
 });
